962468aa8e7f84f9f65ad83d6a227f27879bc08f
ZXM-MD <110195953+ZXM-MD@users.noreply.github.com>
2022-11-16 05:25:31 +0800

    Update config.js

--- config.js
+++ config.js
@@ -39,15 +39,14 @@ const readMore = more.repeat(4001)
 //⫹⫺ ╳╶╼╶╶╶╶┈ ⎝ NO OWNER ⎞ ┈╴╴╴╴╾╴╳ ⫹⫺
 
 global.owner = [
-['6282376267531', 'Aisyah', true],
-['6281361281833', 'Aldi', true]
+["6289636371132", "PAK ZAINAL DEV'S", true],
 ]
 global.mods = []
 global.prems = []
-global.nomorbot = '6283838186170'
-global.nomorown = '6281361281833'
-global.nomorown1 = '6281361281833'
-global.nomorown2 = '6282376267531'
+global.nomorbot = '6289636371132'
+global.nomorown = '6289636371132'
+global.nomorown1 = '6289636371132'
+global.nomorown2 = '6289636371132'
 global.nomorwa = '0'
 
 
